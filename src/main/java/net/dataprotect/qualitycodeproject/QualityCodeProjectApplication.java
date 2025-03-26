f79c9b6b3d9eef5d95831d4ad5564e68744052de
soufiane <mbafianso@gmail.com>
2025-03-26 16:13:45 +0000

    C1

---
+++ src/main/java/net/dataprotect/qualitycodeproject/QualityCodeProjectApplication.java
@@ -0,0 +1,13 @@
+package net.dataprotect.qualitycodeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityCodeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityCodeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/dataprotect/qualitycodeproject/QualityCodeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dataprotect.qualitycodeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityCodeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
